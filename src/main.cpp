3ebfa32b3be5926a6a4c1715d2d42dafaa6f277d
Suleymanov Andrey <sasus37@gmail.com>
2023-05-16 21:25:19 +0300

    add command to start

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,27 @@
 #include <QApplication>
 
 int main() {
+    std::string command;
+    std::cout << "welcome to the search engine!\n:";
+    std::cin>> command;
+    if(command == "start" || command == "START" || command == "Start") {
+        ConverterJson converter;
+        InvertedIndex idx;
+        idx.UpdateDocumentBase(converter.GetTextDocuments());
+        SearchServer currentServer(idx);
+        auto tempResult = currentServer.search(converter.GetRequests(), converter.GetResponsesLimit());
+        std::vector<std::vector<std::pair<int, float>>> searchResult;
+        for (auto tmpPart: tempResult) {
+            std::vector<std::pair<int, float>> searchResultPart;
+            for (auto tmpRltiveIdx: tmpPart) {
+                std::pair<int, float> tmpPair;
 
-    ConverterJson converter;
-    InvertedIndex idx;
-    idx.UpdateDocumentBase(converter.GetTextDocuments());
-    SearchServer currentServer(idx);
-    auto tempResult = currentServer.search(converter.GetRequests(),converter.GetResponsesLimit());
-    std::vector<std::vector<std::pair<int, float>>> searchResult;
-    for(auto tmpPart : tempResult){
-        std::vector <std::pair<int,float>>searchResultPart;
-        for(auto tmpRltiveIdx : tmpPart){
-            std::pair<int,float> tmpPair;
-
-           tmpPair.first = tmpRltiveIdx.doc_id;
-           tmpPair.second = tmpRltiveIdx.rank;
-           searchResultPart.push_back(tmpPair);
+                tmpPair.first = tmpRltiveIdx.doc_id;
+                tmpPair.second = tmpRltiveIdx.rank;
+                searchResultPart.push_back(tmpPair);
+            }
+            searchResult.push_back(searchResultPart);
         }
-    searchResult.push_back(searchResultPart);
+        converter.putAnswers(searchResult);
     }
-    converter.putAnswers(searchResult);
-
 }
